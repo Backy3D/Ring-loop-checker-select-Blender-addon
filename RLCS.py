5fd318feccf5389a0845112c1dd51fd6d70926f3
Backy3D <132197813+Backy3D@users.noreply.github.com>
2023-04-30 13:01:21 +0200

    Add files via upload
    This add-on adds ring loops with a gap between them for easier decimation.
    The function is listed under "select" in edit mode. But i recommend assigning it the shortcut alt+R. As it isn't used in edit mode.
    How it works:
    1. Select a single edge.
    2. Click the button.
    3. You can press control+X to dissolve your selection.
    
    Alternatively the problem can also be solved without this add-on.
    1. select an edge
    2. click select>loops>ring loop.
    3. click select>checker deselect.
    4. click select>loops>edge loop.
    5. You can press control+X to dissolve your selection.
    and you got the same result.

---
+++ RLCS.py
@@ -0,0 +1,69 @@
+bl_info = {
+    "name": "Ring loop checker select",
+    "author": "Backy3D",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D > UI",
+    "description": "Checker selects a cylinder with ring loops",
+    "warning": "",
+    "doc_url": "",
+    "category": "Mesh",
+}
+
+
+
+
+import bpy
+import bpy
+from bpy.types import (
+    AddonPreferences,
+    Operator,
+    Panel,
+    PropertyGroup
+)
+
+
+def main(context):
+    for ob in context.scene.objects:
+        print(ob)
+
+
+class SimpleOperator(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "mesh.simple_operator"
+    bl_label = "Ring loop checker select"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+    
+    def execute(self, context):
+        iterations = 1
+        main(context)
+        bpy.ops.mesh.loop_multi_select(ring=True)
+        bpy.ops.mesh.select_nth(nth = iterations)
+        bpy.ops.mesh.loop_multi_select(ring=False)
+
+        return {'FINISHED'}
+
+
+def menu_func(self, context):
+    self.layout.operator(SimpleOperator.bl_idname, text=SimpleOperator.bl_label)
+
+
+def register():
+    bpy.utils.register_class(SimpleOperator)
+    bpy.types.VIEW3D_MT_select_edit_mesh.append(menu_func)
+
+
+def unregister():
+    bpy.utils.unregister_class(SimpleOperator)
+    bpy.types.VIEW3D_MT_select_edit_mesh.remove(menu_func)
+    
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    #bpy.ops.mesh.simple_operator()
